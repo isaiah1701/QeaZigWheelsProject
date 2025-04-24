294950d35d2752c911cd490dffd6f8e8b118b852
Isaiah Michael <2391446@cognizant.com>
2025-04-24 18:03:18 +0100

    without headless

--- Hooks/Hooks.cs
+++ Hooks/Hooks.cs
@@ -34,7 +34,7 @@ namespace BikeProject.Hooks
             // Add headless mode if running in CI/CD
             if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("HEADLESS")) && Environment.GetEnvironmentVariable("HEADLESS") == "true")
             {
-                options.AddArgument("--headless=new");
+                //options.AddArgument("--headless=new");
                 Console.WriteLine("BeforeScenario: Running Chrome in headless mode.");
             }
 
